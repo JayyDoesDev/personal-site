4134b46736012aa12832fac88a319cd94ef4dfd4
Jdevdisc <69683068+JDevelo@users.noreply.github.com>
2021-12-24 22:07:03 -0500

    rewrite

---
+++ js/keys.js
@@ -0,0 +1,32 @@
+document.onkeydown = function (key) {
+    if (event.keyCode == 123) {
+  
+      return false;
+    }
+  
+    if (key.ctrlKey && key.shiftKey && key.keyCode == "I".charCodeAt(0)) {
+  
+      return false;
+    }
+  
+    if (key.ctrlKey && key.shiftKey && key.keyCode == "C".charCodeAt(0)) {
+  
+      return false;
+    }
+  
+    if (key.ctrlKey && key.shiftKey && key.keyCode == "J".charCodeAt(0)) {
+  
+      return false;
+    }
+  
+    if (key.ctrlKey && key.keyCode == "U".charCodeAt(0)) {
+  
+      return false;
+    }
+  };
+  
+  document.addEventListener("contextmenu", (key) => key.preventDefault());
+  
+  document.addEventListener("developertools", (key) => key.preventDefault());
+  
+  document.addEventListener("inspect_element", (key) => key.preventDefault());
