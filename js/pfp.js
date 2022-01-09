23c5886d9051bf4f2630f903113463836df3ff94
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2022-01-08 20:06:41 -0500

    pfp

---
+++ js/pfp.js
@@ -0,0 +1,11 @@
+
+fetch("https://api.hyplix.com/users/discord/419958345487745035/avatar", {
+    method: "GET"
+}).then((x) => {
+    x.json().then(data => {
+        document.getElementById("pfp").setAttribute("src", data.url)
+    })
+})
+
+
+
