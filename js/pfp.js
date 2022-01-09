2701f857b7bad0dc69b15e3acb40965502dee2d4
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2022-01-09 03:08:19 -0500

    Update pfp.js

--- js/pfp.js
+++ js/pfp.js
@@ -1,12 +1,12 @@
-setInterval(() => {
-    fetch("https://api.hyplix.com/users/discord/419958345487745035/avatar", {
+
+fetch("https://api.hyplix.com/users/discord/419958345487745035/avatar", {
     method: "GET"
 }).then((x) => {
     x.json().then(data => {
         document.getElementById("pfp").setAttribute("src", data.url)
     })
 })
-}, 2000);
+
 
 
 
