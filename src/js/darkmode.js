f000f271db31f5b83142bd8f2d409fcd7fee52fd
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-07-13 04:48:41 -0400

    useless line

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -4,7 +4,6 @@ function darkMode() {
   var element = document.body;
   var text = document.getElementById("darkmode");
   element.classList.toggle("darkmode");
-  element.classList.toggle("theme-light");
   if (text.innerHTML == "light mode") {
     text.innerHTML = "dark mode";
   } else {
