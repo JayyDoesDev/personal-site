c0afe242a5b6972429582b5c144086a2b8379573
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-08-05 05:52:19 -0400

    Better status 💎

--- src/js/presense.js
+++ src/js/presense.js
@@ -9,16 +9,19 @@ async function getPresence() {
 
     switch (status) {
       case "online":
-        statusIcon.style.backgroundColor = "#57f287";
+        statusIcon.src = "../../img/statuses/online.png";
         break;
       case "idle":
-        statusIcon.style.backgroundColor = "#fee75c";
+        statusIcon.src = "../../img/statuses/idle.png";
         break;
       case "dnd":
-        statusIcon.style.backgroundColor = "#ed4245";
+        statusIcon.src = "../../img/statuses/dnd.png";
+        break;
+      case "streaming":
+        statusIcon.src = "../../img/statuses/streaming.png";
         break;
       default:
-        statusIcon.style.backgroundColor = "#23272a";
+        statusIcon.src = "../../img/statuses/offline.png";
     }
   } catch (error) {
     console.error(err);
