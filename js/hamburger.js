e7aaa14753bcb900af1293ff87dfa002897f7dd8
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-06-25 18:49:59 -0400

    feat: Another site rewrite

---
+++ js/hamburger.js
@@ -0,0 +1,4 @@
+function hamburger() {
+    const contents = document.getElementsByClassName("hamburger")[0];
+    contents.style.display == "none" ? contents.style.display = "grid" : contents.style.display = "none"
+}
